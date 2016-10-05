97ed48b95141ba8051a7673168a6d3fa6f0d49d2
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-10-05 18:34:30 -0400

    added goal boundaries

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -15,12 +15,11 @@ public class MoveBall : MonoBehaviour {
 	}
 
 	void Update(){
-		/*float moveX = Input.GetAxis ("Horizontal");
+		float moveX = Input.GetAxis ("Horizontal");
 		float moveZ = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveX, 0.0f, moveZ);
-
-		ball.AddForce (movement * speed);*/
+		ball.AddForce (movement * speed);
 
 	}
 		
