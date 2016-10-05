73fc9275e9b5ad3a17c747c676aa7b5bed4e6d35
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-10-05 17:06:56 -0400

    first commit with field and ball

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody ball;
+
+	void Start(){
+		ball = GetComponent<Rigidbody> ();
+
+		ball.maxAngularVelocity = 20.0f;
+
+	}
+
+	void Update(){
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveX, 0.0f, moveZ);
+
+		ball.AddForce (movement * speed);
+
+	}
+		
+
+}
+
